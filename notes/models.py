2de374250e8daad6bc03990cb63e9df4aa8875ac
Yue <su2047@gmail.com>
2021-04-18 00:44:27 -0300

    added isDone

--- notes/models.py
+++ notes/models.py
@@ -7,6 +7,7 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    isDone = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
